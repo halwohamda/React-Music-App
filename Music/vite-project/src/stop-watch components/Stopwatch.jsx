0c8cf45225308ec9ada4ad6af78dbceb7896437a
halwohamda <halwohamda@gmail.com>
2024-04-24 17:22:11 -0800

    done

--- Music/vite-project/src/stop-watch components/Stopwatch.jsx
+++ Music/vite-project/src/stop-watch components/Stopwatch.jsx
@@ -2,6 +2,10 @@ import React,{useState,useEffect,useRef} from "react";
 
 
 const Stopwatch=()=>{
+
+
+    const [isRunning, setRunning] = useState(false);
+    cosnt [elapsedTime, setElapsedTime] = useState(0);
     return(
         <div>
 
